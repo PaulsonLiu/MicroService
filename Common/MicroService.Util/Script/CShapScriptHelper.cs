055b7a5fb4cc56eea8916240fbccf99fe6f4fdbd
Paulson <paulsonliu@163.com>
2017-08-08 14:39:13 +0800

    Add Roslyn Script Helper

---
+++ Common/MicroService.Util/Script/CShapScriptHelper.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MicroService.Util
+{
+    public class CShapScriptHelper
+    {
+        #region core不支持
+        //private readonly string mainCalssName = "ParseScript";
+        //public string ErrorMsg { get; set; }
+        //public CompilerResults complierResult { get; set; }
+        //public bool HasErrors
+        //{
+        //    get
+        //    {
+        //        return complierResult.Errors.HasErrors;
+        //    }
+        //}
+        //public ScriptParseHelper(string script, Dictionary<string, string> golbalVariableDic = null)
+        //{
+        //    string golbalVariableStr = "";
+        //    if (golbalVariableDic != null)
+        //    {
+        //        foreach (var item in golbalVariableDic)
+        //        {
+        //            golbalVariableStr += string.Format("public string {0} = \"{1}\";", item.Key, item.Value);
+        //        }
+        //    }
+        //    var source =
+        //        @"using System;
+        //            public class " + mainCalssName + @"{
+        //            " + golbalVariableStr + script + @"
+        //        }";
+        //    // 1.Create a new CSharpCodePrivoder instance
+        //    CSharpCodeProvider objCSharpCodePrivoder = new CSharpCodeProvider();
+        //    // 2.Sets the runtime compiling parameters by crating a new CompilerParameters instance
+        //    CompilerParameters objCompilerParameters = new CompilerParameters();
+        //    objCompilerParameters.ReferencedAssemblies.Add("System.dll");
+        //    objCompilerParameters.GenerateInMemory = true;
+        //    // 3.CompilerResults: Complile the code snippet by calling a method from the provider
+        //    CompilerResults cr = objCSharpCodePrivoder.CompileAssemblyFromSource(objCompilerParameters, source);
+        //    if (cr.Errors.HasErrors)
+        //    {
+        //        string strErrorMsg = cr.Errors.Count.ToString() + " Errors:";
+        //        for (int x = 0; x < cr.Errors.Count; x++)
+        //        {
+        //            strErrorMsg = strErrorMsg + "/r/nLine: " +
+        //                cr.Errors[x].Line.ToString() + " - " +
+        //                cr.Errors[x].ErrorText;
+        //        }
+        //        ErrorMsg = strErrorMsg;
+        //    }
+        //    complierResult = cr;
+        //}
+
+        //public object InvokeMethod(string methodName, object[] objCodeParms)
+        //{
+        //    if (complierResult != null && !this.HasErrors)
+        //    {
+        //        // 4. Invoke the method by using Reflection
+        //        Assembly objAssembly = complierResult.CompiledAssembly;
+        //        object objClass = objAssembly.CreateInstance(mainCalssName);
+        //        if (objClass == null)
+        //        {
+        //            ErrorMsg = "Error: " + "Couldn't load class ParseScriptHelper.";
+        //            return false;
+        //        }
+        //        //input parmeters
+        //        //object[] objCodeParms = new object[1];
+        //        //objCodeParms[0] = "Allan.";
+        //        object result = objClass.GetType().InvokeMember(methodName, BindingFlags.InvokeMethod, null, objClass, objCodeParms);
+        //        return result;
+        //    }
+        //    return null;
+        //}
+        #endregion
+    }
+}
---
+++ Common/MicroService.Util/Script/RoslynScriptHelper.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+/// <summary>
+/// 参考：http://www.cnblogs.com/TianFang/p/6939723.html
+/// </summary>
+namespace MicroService.Util
+{
+    public class RoslynScriptHelper
+    {
+        private Script<object> Script { get; set; }
+        private Globals _Globals { get; set; }
+        public RoslynScriptHelper(string script, Globals globals)
+        {
+            _Globals = globals;
+            Script = CSharpScript.Create(script,globalsType:typeof(Globals));
+            Script.Compile();//编译脚本
+        }
+
+
+        public async Task<object> RunAsync()
+        {
+            return await Script.RunAsync(_Globals);
+        }
+
+        //异常处理
+        private async void runScript()
+        {
+            try
+            {
+                Console.WriteLine(await CSharpScript.EvaluateAsync("2+2"));
+            }
+            catch (CompilationErrorException e)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, e.Diagnostics));
+            }
+        }
+
+        //带上下文状态执行
+        private async void RunContext()
+        {
+            var state = await CSharpScript.RunAsync("int x = 1;");
+            state = await state.ContinueWithAsync("int y = 2;");
+            state = await state.ContinueWithAsync("x+y");
+            Console.WriteLine(state.ReturnValue);
+        }
+
+        //添加程序集引用
+        private async void WithReferences()
+        {
+            var result = await CSharpScript.EvaluateAsync("System.Net.Dns.GetHostName()",
+                                ScriptOptions.Default.WithReferences(typeof(System.Net.Dns).AssemblyQualifiedName));
+        }
+
+        //添加using导入
+        private async void WithImports()
+        {
+            var result = await CSharpScript.EvaluateAsync("Sqrt(2)",
+                                ScriptOptions.Default.WithImports("System.Math"));
+        }
+
+        //和属猪程序中的对象交互
+        public async Task<object> EvaluateAsync(string scripts) {
+            var globals = new Globals { Version = "1.0.0", Company = "ii" };
+            return await CSharpScript.EvaluateAsync(scripts, globals: globals);
+        }
+
+    }
+
+    public class Globals
+    {
+        public string Version;
+        public string Company;
+    }
+
+}
